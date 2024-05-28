c5aa74e8902750c01d686709193d5798d2dc5819
21mb358@esisalama.org <21mb358@esisalama.org>
2024-05-28 12:37:21 +0200

    ajout de fichier MarsPhotoService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 
+
 }
 
 android {
@@ -79,8 +80,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    // Retrofit with Kotlin serialization Converter
+
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
 
 
 
--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,23 +1,30 @@
 package com.example.marsphotos.network
-
+import com.example.marsphotos.model.MarsPhoto
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
     .baseUrl(BASE_URL)
     .build()
+
+/**
+ * Retrofit service object for creating api calls
+ */
 interface MarsApiService {
     @GET("photos")
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 }
+
+
 object MarsApi {
-    val retrofitService : MarsApiService by lazy {
+    val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
 }
-
-
